ae2071e41f0dd6f3779b3c82820edafe9de6f757
yurika sakae <yurika.sun@gmail.com>
2019-08-16 09:29:10 -0700

    Initial commit WIP: setting up Redux

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  smurfs: [],
+  isLoading: false,
+  error: ""
+};
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
